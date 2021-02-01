257fe6788c868cd0d30059b1f7288f76c1ffeac0
Brishen <brishen.hawkins@gmail.com>
2021-02-01 13:27:37 -0700

    Create install_clock.sh

---
+++ rtc/install_clock.sh
@@ -0,0 +1,5 @@
+sudo echo "dtoverlay=i2c-rtc,pcf2129" >> /boot/firmware.config.txt
+sudo rm /lib/systemd/system/hwclock.service
+sudo cp hwclock.service /lib/systemd/system/hwclock.service
+sudo systemctl daemon-reload
+sudo systemctl enable hwclock
